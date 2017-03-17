45ad1d5787321bd98f066d0490641d53130572a6
CharlesCarlstrom <cc113884@students.davenport.k12.ia.us>
2017-03-17 11:23:54 -0500

    update

--- src/adventure/Tavern.java
+++ src/adventure/Tavern.java
@@ -20,22 +20,22 @@ public class Tavern {
 		JOptionPane.showMessageDialog(null, " You head to the tavern and along the way you see some people on and you ask them how much further the tavern is.");
 		JOptionPane.showMessageDialog(null, " they drop everything they're carrying and run from you");
 
-		Object[] options = {"Kings Castle",
-        "Oceanside Tavern"};
+		Object[] options = {"Steal",
+        "Continue to the Tavern"};
 		Component frame = null;
-		int CastleOrTavern= JOptionPane.showOptionDialog(frame,
-				"Kings Castle or Oceanside Tavern",
-				"Gunpowder Question",
+		int StealOrTavern= JOptionPane.showOptionDialog(frame,
+				"Search their things or continue on",
+				"Question",
 				JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
 				null,  
 				options,  
 				options[0]); 
-		if(CastleOrTavern == JOptionPane.YES_OPTION) // King's Castle. You Die in this one.
+		if(StealOrTavern == JOptionPane.YES_OPTION) // you steal some stuff the go to the tavern.
 		{
 			 Castle.castlePath(userName);
 		}
-		if(CastleOrTavern == JOptionPane.NO_OPTION) // you go to the tavern.
+		if(StealOrTavern == JOptionPane.NO_OPTION) // you go to the tavern.
 		{
 			Tavern.tavernPath(userName);
 		}
